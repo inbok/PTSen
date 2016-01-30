73611d41c184751b8fb19ac52e482cc4a4ba7c7d
인복 장 <ibjangb@gmail.com>
2016-01-30 21:57:07 +0900

    단위테스트 프로젝트 생성

---
+++ ProtocolAnalysisUsingSentinel/ProtocolAnalysisUsingSentinelTests/Form1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtocolAnalysisUsingSentinel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolAnalysisUsingSentinel.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void testCodeTest()
+        {
+            Form1 testForm = new Form1();
+            Assert.AreEqual(true, testForm.testCode());
+        }
+    }
+}
